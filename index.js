6dc69d407c7cbf6fd9f4859eca9911587e74fb2d
Renan Oliveira <renan.sergipe68@gmail.com>
2021-03-24 17:47:46 -0300

    Arquitetura e organização de aplicação Node.js

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const express = require("express");
 const app = express();
 const connection = require("./database/dataBase");
-
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/articlesController");
 
 app.use(express.urlencoded({ extended: true }));
 //app.use(express.json);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-
 connection
- .authenticate()
- .then(() => {
+  .authenticate()
+  .then(() => {
     console.log("Conexão estabelecida!");
- })
+  })
   .catch((erro) => {
-   console.log("Erro!");
-});
+    console.log("Erro!");
+  });
+
+app.use("/", categoriesController);
+app.use("/", articlesController);
 
 app.get("/", (req, res) => {
-  res.render('index.ejs')
+  res.render("index.ejs");
 });
 
 app.listen(8080, (erro) => {
