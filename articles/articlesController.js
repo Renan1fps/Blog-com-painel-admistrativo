8327505340b06e5cb821c464dee06ba3becbdcdd
Renan Oliveira <renan.sergipe68@gmail.com>
2021-03-26 17:48:10 -0300

    Adicionando TinyMCE(editor de texto) e configurando

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const Article= require("./Article")
+const Article = require("./Article");
+const slugify = require("slugify");
 
 router.get("/admin/articles/new", (req, res) => {
-  res.render("admin/articles/new")
+  res.render("admin/articles/new");
 });
+
 
 module.exports = router;
