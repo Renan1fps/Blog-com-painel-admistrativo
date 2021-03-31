2155e1a7834fb428efa3fd3a5068c6035cc94039
Renan Oliveira <renan.sergipe68@gmail.com>
2021-03-31 14:56:13 -0300

    Filtrando artigos por categoria

--- index.js
+++ index.js
@@ -54,6 +54,32 @@ app.get("/:slug", (req, res) => {
     });
 });
 
+app.get("/category/:slug", (req, res) => {
+  var slug = req.params.slug;
+  Category.findOne({
+    where: {
+      slug: slug,
+    },
+    include: [{ model: Article }]
+  }).then((category) => {
+      if (category != undefined) {
+        Category.findAll().then(categories=>{
+          res.render("index", {
+            articles: category.articles,
+            categories: categories,
+          })
+        });
+       
+      } else {
+        res.redirect("/");
+       
+      }
+    })
+    .catch((error) => {
+      res.redirect("/");
+    });
+});
+
 app.listen(8080, (erro) => {
   if (!erro) {
     console.log("Servidor iniciado!");
